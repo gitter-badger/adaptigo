6c56c2238d769cf89350a8708f301d022fe5955b
Dick Thomas <xpd259@gmail.com>
2015-08-19 00:03:01 +0100

    created crashplan plugin

---
+++ plugins/Crashplan.plugin/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+CACHEDIR="/var/cache/debi/CrashPlan/"
+
+
+
+mkdir -p "$CACHEDIR"
+cd "$CACHEDIR"
+
+URL="https://download.code42.com/installs/linux/install/CrashPlan/CrashPlan_4.3.0_Linux.tgz"
+FILE="CrashPlan_4.3.0_Linux.tgz"
+
+wget -c "$URL" -O "$FILE"
+
+if [[ ! -f "$FILE" ]]; then
+	exit 1
+fi
+
+dir=`mktemp -d` && cd $dir
+tar xvf "$CACHEDIR/$FILE"
+x-terminal-emulator -e $dir/CrashPlan-install/install.sh
+
+xdg-icon-resource install --novendor --size 128 "/usr/local/crashplan/skin/icon_app_128x128.png" "CrashPlan"
+gtk-update-icon-cache -f -t /usr/share/icons/hicolor
+
+cat <<EOF | tee /usr/share/applications/CrashPlan.desktop
+[Desktop Entry]
+Name=CrashPlan
+Icon=CrashPlan
+Comment=Backup your pc
+Exec=CrashPlanDesktop
+Terminal=false
+Type=Application
+StartupNotify=true
+Categories=
+Keywords=Backup
+EOF
