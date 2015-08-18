037c5c9131ab0bc7aff66bdb4d5a3da7eef3e9d5
Dick Thomas <xpd259@gmail.com>
2015-08-18 23:07:49 +0100

    typos

--- plugins/chrome386.plugin/install.sh
+++ plugins/chrome386.plugin/install.sh
@@ -2,4 +2,4 @@
 aptitude insatll -y apt-transport-https
 echo 'deb https://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-aptitude insatll -y google-chrome-stable
+aptitude install -y google-chrome-stable
