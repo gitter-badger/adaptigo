959a535385f99f6f179e759e1ab0c32214c328b4
Dick Thomas <xpd259@gmail.com>
2015-08-18 23:08:27 +0100

    oops

--- plugins/chrome386.plugin/install.sh
+++ plugins/chrome386.plugin/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-aptitude insatll -y apt-transport-https
+aptitude install -y apt-transport-https
 echo 'deb https://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 aptitude install -y google-chrome-stable
