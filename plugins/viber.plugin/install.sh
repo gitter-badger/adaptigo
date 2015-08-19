f73df5ea331f96be4822f8ed0b948cf47f09ce5e
Dick Thomas <xpd259@gmail.com>
2015-08-19 23:20:22 +0100

    seems viber is i386 so added dpkg --add-architecture i386

--- plugins/viber.plugin/install.sh
+++ plugins/viber.plugin/install.sh
@@ -1,6 +1,8 @@
 
 #!/bin/bash
 aptitude install -y curl
-http://download.cdn.viber.com/cdn/desktop/Linux/viber.deb -o viber.deb
+dpkg --add-architecture i386
+aptitude update
+curl http://download.cdn.viber.com/cdn/desktop/Linux/viber.deb -o viber.deb
 dpkg -i viber.deb
 apt-get --yes --fix-broken install
