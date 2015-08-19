5c785283385fd88e5c67de210252ddb0ee5c2c98
Dick Thomas <xpd259@gmail.com>
2015-08-19 12:27:39 +0100

    added new install and query and remove

--- plugins/codecs.plugin/install.sh
+++ plugins/codecs.plugin/install.sh
@@ -3,4 +3,4 @@ aptitude install -y apt-transport-https
 echo "deb http://download.videolan.org/pub/debian/stable/ /"> /etc/apt-sources.d/videolan.list
 wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc | sudo apt-key add -
 aptitude update
-apt-get install libavcodec-extra-54 libdvdcss2
+apt-get install -y libavcodec-extra libdvdcss2
