1704644c308387e4f27ba6f2dec27dda8bef875d
Dick Thomas <xpd259@gmail.com>
2015-08-19 17:50:18 +0100

    updated grub to use debian update-grub2

--- plugins/grub2.plugin/setup.sh
+++ plugins/grub2.plugin/setup.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-if [[ -d /boot/efi/EFI/fedora ]]; then
-    grub2-mkconfig -o /boot/efi/EFI/fedora/grub.cfg
-elif [[ -d /boot/grub2 ]]; then
-    grub2-mkconfig -o /boot/grub2/grub.cfg
-else
-    exit 1
-fi
+update-grub2y
+
