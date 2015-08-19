58cf0e28d71102595fe7925dac0607e913ea5e23
Dick Thomas <xpd259@gmail.com>
2015-08-19 20:24:30 +0100

    python script rejig

--- plugins/flash.plugin/install.sh
+++ plugins/flash.plugin/install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-
-if [ ! -f /etc/apt/sources.list.original ]; then
-cp /etc/apt/sources.list /etc/apt/sources.list.original
-sed -e 's/(debian/ jessie main)/debian/ jessie main non-free contrib/g' /etc/apt/sources.list
-
-fi
+python rejig.py
+aptitude update
 aptitude install -y flashplugin-nonfree
