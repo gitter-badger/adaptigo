7e77b7490b5f0501b09e39d4a37f15dc4cd1b3f5
Dick Thomas <xpd259@gmail.com>
2015-08-19 23:57:53 +0100

    initial teamviewer

--- plugins/teamviewer.plugin/install.sh
+++ plugins/teamviewer.plugin/install.sh
@@ -2,18 +2,22 @@
 
 CACHEDIR="/var/cache/fedy/teamviewer"
 
-mkdir -p "$CACHEDIR"
-cd "$CACHEDIR"
-
-curl http://download.teamviewer.com/download/teamviewer_i386.deb -o teamviewer.deb
-
-
-wget -c "$URL" -O "$FILE"
-
 if [[ ! -f "$FILE" ]]; then
 	exit 1
 fi
 
-dpkg --add-architecture i386
-dpkg -i install teamviewer.deb
+
 apt-get --yes --fix-broken install
+if $(uname -a | grep 'x86_64')
+	then
+ 
+  	wget http://download.teamviewer.com/download/teamviewer_i386.deb
+	dpkg -i install teamviewer_i386.deb
+	else
+
+  wget http://download.teamviewer.com/download/teamviewer_amd64.deb
+  dpkg -i install teamviewer_amd64.deb
+
+fi
+
+
