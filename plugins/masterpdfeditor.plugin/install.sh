7a6fac8752b1b94c3da69a8bc337181643f5c624
Dick Thomas <xpd259@gmail.com>
2015-08-19 22:33:01 +0100

    amd64 vs 386 installer

--- plugins/masterpdfeditor.plugin/install.sh
+++ plugins/masterpdfeditor.plugin/install.sh
@@ -1,15 +1,14 @@
+
 #!/bin/bash
-MACHINE_TYPE=`uname -m`
-if [ ${MACHINE_TYPE} == 'x86_64' ]; then
+if $(uname -a | grep 'x86_64'); then
+
   # 64-bit stuff here
-  wget http://get.code-industry.net/public/master-pdf-editor-3.3.10_amd64.deb
-  dpkg -i master-pdf-editor-3.3.10_amd64.deb
-  apt-get --yes --fix-broken install
+  curl http://get.code-industry.net/public/master-pdf-editor-3.3.10_amd64.deb -o master-pdf-editor.deb
 else
   # 32-bit stuff here
-  http://get.code-industry.net/public/master-pdf-editor-3.3.10_i386.deb
-  dpkg -i master-pdf-editor-3.3.10_i386.deb
-  apt-get --yes --fix-broken install
-else
+  curl http://get.code-industry.net/public/master-pdf-editor-3.3.10_i386.deb -o master-pdf-editor.deb
+  
 fi
 
+dpkg -i master-pdf-editor.deb
+apt-get --yes --fix-broken install
