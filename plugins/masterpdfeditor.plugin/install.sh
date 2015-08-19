ac14f2b7d1c6bae8577dbe9b427ad86a1d1a48ce
Dick Thomas <xpd259@gmail.com>
2015-08-19 22:56:56 +0100

    finishd PDF master and started mscorefonts

--- plugins/masterpdfeditor.plugin/install.sh
+++ plugins/masterpdfeditor.plugin/install.sh
@@ -2,13 +2,13 @@
 #!/bin/bash
 aptitude install -y curl
 if $(uname -a | grep 'x86_64'); then
-
+ # 32-bit stuff here
+  curl http://get.code-industry.net/public/master-pdf-editor-3.3.10_i386.deb -o master-pdf-editor.deb
+ 
+else
+  
   # 64-bit stuff here
   curl http://get.code-industry.net/public/master-pdf-editor-3.3.10_amd64.deb -o master-pdf-editor.deb
-else
-  # 32-bit stuff here
-  curl http://get.code-industry.net/public/master-pdf-editor-3.3.10_i386.deb -o master-pdf-editor.deb
-  
 fi
 
 dpkg -i master-pdf-editor.deb
