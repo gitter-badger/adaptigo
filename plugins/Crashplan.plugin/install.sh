23305c5f0193d362c6e74d9294123332bf6af0c5
Dick Thomas <xpd259@gmail.com>
2015-08-19 11:16:28 +0100

    I think I've fixed the crashplan install

--- plugins/Crashplan.plugin/install.sh
+++ plugins/Crashplan.plugin/install.sh
@@ -3,7 +3,7 @@
 CACHEDIR="/var/cache/debi/CrashPlan/"
 mkdir -p "$CACHEDIR"
 cd "$CACHEDIR"
-echo "to install run ./install" > guide.txt
+echo "to install run ./install in the Terminal" > guide.txt
 URL="https://download.code42.com/installs/linux/install/CrashPlan/CrashPlan_4.3.0_Linux.tgz"
 FILE="CrashPlan_4.3.0_Linux.tgz"
 
@@ -16,8 +16,11 @@ fi
 #dir=`mktemp -d` && cd $dir
 tar xvf "$FILE"
 export DISPLAY=":0.0" 
-xterm -e "$CACHEDIR"/CrashPlan-install/install.sh
-gnome-terminal -e gedit guide.txt && bash 
+
+xterm -e gedit "$CACHEDIR"guide.txt
+xterm -e bash
+
+
 xdg-icon-resource install --novendor --size 128 "/usr/local/crashplan/skin/icon_app_128x128.png" "CrashPlan"
 gtk-update-icon-cache -f -t /usr/share/icons/hicolor
 
