e732a1b52d3869411358affbfc1b719419d3730b
Dick Thomas <xpd259@gmail.com>
2015-08-19 21:20:36 +0100

    updated flash this should be the last commit ...

--- plugins/flash.plugin/install.sh
+++ plugins/flash.plugin/install.sh
@@ -8,12 +8,16 @@ then
 	aptitude update
 	aptitude install -y flashplugin-nonfree
 else
+	cp /etc/apt/sources.list /etc/apt/sources.list.backup
 	sed -i 's|debian/ jessie main|debian/ jessie main non-free contrib|g' /etc/apt/sources.list
 
 	sed -i 's|jessie/updates main|jessie/updates main non-free contrib|g' /etc/apt/sources.list
 
-	sed -i 's|debian/ jessie-updates main|debian/ jessie-updates main non-free contrib' /etc/apt/sources.list
+	sed -i 's|debian/ jessie-updates main|debian/ jessie-updates main non-free contrib|g' /etc/apt/sources.list
+	aptitude update
+	aptitude install -y flashplugin-nonfree
 fi
+
 
 
 
