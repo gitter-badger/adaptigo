1823e5dd38f210d608ce91b2a09ba7deea7fee65
Dick Thomas <xpd259@gmail.com>
2015-08-19 17:47:20 +0100

    delted evopop and edited font

--- plugins/fontconfig.plugin/setup.sh
+++ plugins/fontconfig.plugin/setup.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-run-as-root dnf -y install ozon-font-config
-
 gsettings set org.gnome.settings-daemon.plugins.xsettings antialiasing "rgba"
 gsettings set org.gnome.settings-daemon.plugins.xsettings hinting "slight"
