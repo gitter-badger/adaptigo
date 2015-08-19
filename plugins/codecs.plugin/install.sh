989df35e2f8f01ad2d107d647042dab18ef2966e
Dick Thomas <xpd259@gmail.com>
2015-08-19 12:19:59 +0100

    started codecs plugin

---
+++ plugins/codecs.plugin/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+aptitude install -y apt-transport-https
+echo "deb http://download.videolan.org/pub/debian/stable/ /"> /etc/apt-sources.d/videolan.list
+wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc | sudo apt-key add -
+aptitude update
+apt-get install libavcodec-extra-54 libdvdcss2
