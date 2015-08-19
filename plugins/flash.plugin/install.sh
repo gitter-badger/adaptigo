1525880fb41115576b53daa6f24de4f000cb9671
Dick Thomas <xpd259@gmail.com>
2015-08-19 20:54:18 +0100

    I think I've got a working upgrade to non free etc

--- plugins/flash.plugin/install.sh
+++ plugins/flash.plugin/install.sh
@@ -8,8 +8,13 @@ then
 	aptitude update
 	aptitude install -y flashplugin-nonfree
 else
-	sed 's|debian/ jessie main|debian/ jessie main non-free contrib|g' /etc/apt/sources.list
+	sed -i 's|debian/ jessie main|debian/ jessie main non-free contrib|g' /etc/apt/sources.list
+
+	sed -i 's|jessie/updates main|jessie/updates main non-free contrib|g' /etc/apt/sources.list
+
+	sed -i 's|debian/ jessie-updates main|debian/ jessie-updates main non-free contrib' /etc/apt/sources.list
 fi
+
 
 
 
