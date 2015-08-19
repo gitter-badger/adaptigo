6a6ec53ce96008f68a361642d4163cc91fbe98fd
Dick Thomas <xpd259@gmail.com>
2015-08-19 19:49:49 +0100

    testing  of sed

--- plugins/flash.plugin/install.sh
+++ plugins/flash.plugin/install.sh
@@ -2,6 +2,7 @@
 
 if [ ! -f /etc/apt/sources.list.original ]; then
 cp /etc/apt/sources.list /etc/apt/sources.list.original
-sed -i 's/main/main non-free contrib/g' /etc/apt/sources.list
+sed -e 's/(debian/ jessie main)/debian/ jessie main non-free contrib/g' /etc/apt/sources.list
+
 fi
 aptitude install -y flashplugin-nonfree
