9c4a2d02e8741a4a0654a0de1f3c36b9913405c6
Dick Thomas <xpd259@gmail.com>
2015-08-19 23:34:36 +0100

    finished viber, will test later. fixed typo in codecs

--- plugins/codecs.plugin/install.sh
+++ plugins/codecs.plugin/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-aptitude install -y apt-transport-https
-echo "deb http://download.videolan.org/pub/debian/stable/ /"> /etc/apt-sources.d/videolan.list
+echo "deb http://download.videolan.org/pub/debian/stable/ /" > /etc/apt-sources.list.d/videolan.list
 wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc | sudo apt-key add -
 aptitude update
 apt-get install -y libavcodec-extra libdvdcss2
--- plugins/viber.plugin/install.sh
+++ plugins/viber.plugin/install.sh
@@ -1,8 +1,5 @@
 
 #!/bin/bash
-aptitude install -y curl
-dpkg --add-architecture i386
-aptitude update
 curl http://download.cdn.viber.com/cdn/desktop/Linux/viber.deb -o viber.deb
 dpkg -i viber.deb
 apt-get --yes --fix-broken install
