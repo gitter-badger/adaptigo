ff1a143a4fb11e8e78a2e9919ebf031813683b9a
Dick Thomas <xpd259@gmail.com>
2015-08-19 11:11:55 +0100

    crashplan

--- plugins/Crashplan.plugin/install.sh
+++ plugins/Crashplan.plugin/install.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 CACHEDIR="/var/cache/debi/CrashPlan/"
-
-
-
 mkdir -p "$CACHEDIR"
 cd "$CACHEDIR"
-
+echo "to install run ./install" > guide.txt
 URL="https://download.code42.com/installs/linux/install/CrashPlan/CrashPlan_4.3.0_Linux.tgz"
 FILE="CrashPlan_4.3.0_Linux.tgz"
 
@@ -19,8 +16,8 @@ fi
 #dir=`mktemp -d` && cd $dir
 tar xvf "$FILE"
 export DISPLAY=":0.0" 
-xterm -e $dir/CrashPlan-install/install.sh
-
+xterm -e "$CACHEDIR"/CrashPlan-install/install.sh
+gnome-terminal -e gedit guide.txt && bash 
 xdg-icon-resource install --novendor --size 128 "/usr/local/crashplan/skin/icon_app_128x128.png" "CrashPlan"
 gtk-update-icon-cache -f -t /usr/share/icons/hicolor
 
