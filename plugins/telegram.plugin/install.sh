737943a8b4a936748cc30190e2a5bd398b022e5e
Dick Thomas <xpd259@gmail.com>
2015-08-20 00:30:54 +0100

    delted the grub2 as it's useless and updated telegram

--- plugins/telegram.plugin/install.sh
+++ plugins/telegram.plugin/install.sh
@@ -9,14 +9,16 @@ if (uname -a | grep 'x86_64'); then
 	mv linux32 linux32.tar.gz
   	tar xvf linux.tar.gz
 	rm linux.tar.gz -fv
+	/opt/Telegram/Telegram
 else
    # 32-bit stuff here
     cd /opt
 	wget https://tdesktop.com/linux32 
 	mv linux32 linux32.tar.gz
-	tar xvf linux.tar.gz
+	tar xvf linux32.tar.gz
 	rm linux32.tar.gz -fv
-el
+	/opt/Telegram/Telegram
+
 
 fi
 
