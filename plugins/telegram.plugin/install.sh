3fc4aba85778d5cb0fb4476e0eec041668a7eeff
Dick Thomas <xpd259@gmail.com>
2015-08-20 00:24:02 +0100

    initial telegram and finished teamviewer

--- plugins/telegram.plugin/install.sh
+++ plugins/telegram.plugin/install.sh
@@ -1,5 +1,22 @@
+
 #!/bin/bash
+aptitude install -y curl
+if (uname -a | grep 'x86_64'); then
+ 
+  # 64-bit stuff here
+  cd /opt
+  	wget https://tdesktop.com/linux 
+	mv linux32 linux32.tar.gz
+  	tar xvf linux.tar.gz
+	rm linux.tar.gz -fv
+else
+   # 32-bit stuff here
+    cd /opt
+	wget https://tdesktop.com/linux32 
+	mv linux32 linux32.tar.gz
+	tar xvf linux.tar.gz
+	rm linux32.tar.gz -fv
+el
 
-dnf copr -y enable rommon/telegram
+fi
 
-dnf -y install telegram-desktop
--- plugins/telegram.plugin/uninstall.sh
+++ plugins/telegram.plugin/uninstall.sh
@@ -1,5 +1,5 @@
+
 #!/bin/bash
 
-dnf copr -y disable rommon/telegram
-
-dnf -y --setopt clean_requirements_on_remove=1 erase telegram-desktop
+aptitude -y purge master-pdf-editor3:i386
+aptitude -y purge master-pdf-editor3
