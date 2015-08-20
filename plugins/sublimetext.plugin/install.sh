188bd0d0bf3bbaa143a1f488db687c441b34a6a5
Dick Thomas <xpd259@gmail.com>
2015-08-20 12:45:55 +0100

    sublime finished

--- plugins/sublimetext.plugin/install.sh
+++ plugins/sublimetext.plugin/install.sh
@@ -3,15 +3,15 @@
 CACHEDIR="/var/cache/fedy/sublimetext"
 
 if [[ "$(uname -m)" = "x86_64" ]]; then
-	ARCH="x64"
+	ARCH="amd64"
 else
-	ARCH="x32"
+	ARCH="i386"
 fi
 
 mkdir -p "$CACHEDIR"
 cd "$CACHEDIR"
 
-URL="http://c758482.r82.cf2.rackcdn.com/sublime_text_3_build_3083_${ARCH}.tar.bz2"
+URL="http://c758482.r82.cf2.rackcdn.com/sublime-text_build-3083_$ARCH.deb"
 FILE=${URL##*/}
 
 wget -c "$URL" -O "$FILE"
@@ -20,35 +20,4 @@ if [[ ! -f "$FILE" ]]; then
 	exit 1
 fi
 
-tar -xjf "$FILE"
-
-if [[ ! -d "sublime_text_3" ]]; then
-	exit 1
-fi
-
-rm -rf "/opt/sublime_text_3/"
-cp -af "$CACHEDIR/sublime_text_3" "/opt/"
-
-for dir in /opt/sublime_text_3/Icon/*; do
-    size="${dir##*/}"
-    xdg-icon-resource install --novendor --size "${size/x*}" "$dir/sublime-text.png" "sublime-text"
-done
-
-gtk-update-icon-cache -f -t /usr/share/icons/hicolor
-
-ln -sf /opt/sublime_text_3/sublime_text /usr/local/bin/subl
-
-cat <<EOF | tee /usr/share/applications/sublime-text-3.desktop
-[Desktop Entry]
-Name=Sublime Text 3
-GenericName=Text Editor
-Icon=sublime-text
-Comment=Sophisticated text editor \for code, markup and prose
-Exec=subl %F
-MimeType=text/plain;
-Terminal=false
-Type=Application
-StartupNotify=true
-Categories=Development;Utility;TextEditor;
-Keywords=Text;Editor;
-EOF
+dpkg -i $FILE
