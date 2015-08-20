b8d6b8ce7658c88085f7f91e4cf8e919fb519616
Dick Thomas <xpd259@gmail.com>
2015-08-20 12:21:07 +0100

    added x86 and i686 sections to the insatll script

--- plugins/spotify.plugin/install.sh
+++ plugins/spotify.plugin/install.sh
@@ -1,13 +1,25 @@
 #!/bin/bash
 
-# 1. Add the Spotify repository signing key to be able to verify downloaded packages
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+if (uname -a | grep 'x86_64'); then
+ 
+	wget http://ftp.acc.umu.se/mirror/cdimage/snapshot/Debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.4-3_amd64.deb
+	dpkg -i libgcrypt11_1.5.4-3_amd64.deb
+
+else
+	wget http://ftp.acc.umu.se/mirror/cdimage/snapshot/Debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.4-3_i386.deb
+	dpkg -i libgcrypt11_1.5.4-3_i386.deb
+fi
+
+
+
+
+# 1. Add the Spotify repository signing key to be able to verify downloaded packages
 
 # 2. Add the Spotify repository
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-wget http://ftp.acc.umu.se/mirror/cdimage/snapshot/Debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.4-3_amd64.deb
-dpkg -i libgcrypt11_1.5.4-3_amd64.deb
-# 3. Update list of available packages
+
 sudo apt-get update
 
 # 4. Install Spotify
