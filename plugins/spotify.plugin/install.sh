0a6558e3a6b2d6025e2bfc7110b312ff582e9c94
Dick Thomas <xpd259@gmail.com>
2015-08-20 12:17:25 +0100

    spotify client updated

--- plugins/spotify.plugin/install.sh
+++ plugins/spotify.plugin/install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-dnf config-manager --add-repo=http://negativo17.org/repos/fedora-spotify.repo
-dnf -y install spotify-client
+# 1. Add the Spotify repository signing key to be able to verify downloaded packages
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
+
+# 2. Add the Spotify repository
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+wget http://ftp.acc.umu.se/mirror/cdimage/snapshot/Debian/pool/main/libg/libgcrypt11/libgcrypt11_1.5.4-3_amd64.deb
+dpkg -i libgcrypt11_1.5.4-3_amd64.deb
+# 3. Update list of available packages
+sudo apt-get update
+
+# 4. Install Spotify
+sudo apt-get install spotify-client
--- plugins/spotify.plugin/uninstall.sh
+++ plugins/spotify.plugin/uninstall.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-dnf -y --setopt clean_requirements_on_remove=1 erase spotify-client
-rm -f /etc/yum.repos.d/fedora-spotify.repo
+aptitude purge  -y spotify-client
