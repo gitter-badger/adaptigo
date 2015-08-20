21340f0df1db811249a9d0016f3055f5b6cc5289
Dick Thomas <xpd259@gmail.com>
2015-08-20 14:01:41 +0100

    wps office added

--- plugins/wpsoffice.plugin/install.sh
+++ plugins/wpsoffice.plugin/install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+##http://wps-community.org/downloads
 
-URL=$(wget "http://wps-community.org/download.html" -O - | tr ' ' '\n' | grep -o "https\?://.*/wps-office-.*$(uname -m).rpm" | head -n 1)
 
-if [[ "$URL" != "" ]]; then
-	dnf -y install "$URL"
+aptitude install -y curl
+if (uname -a | grep 'x86_64'); then
+ 
+  # 64-bit stuff here
+  curl http://kdl.cc.ksosoft.com/wps-community/download/a19/wps-office_9.1.0.4975~a19p1_amd64.deb -o wps-office.deb
+
 else
-	exit 1
+   # 32-bit stuff here
+  curl http://kdl.cc.ksosoft.com/wps-community/download/a19/wps-office_9.1.0.4975~a19p1_i386.deb -o wps-office.deb
 fi
+
+dpkg -i wps-office.deb
+apt-get --yes --fix-broken install
