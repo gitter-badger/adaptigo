0ec750ade23c008b5a68cf1a012e3149ddcfd3fb
Dick Thomas <xpd259@gmail.com>
2015-08-20 11:08:15 +0100

    uninstaller steam finished

--- plugins/steam.plugin/uninstall.sh
+++ plugins/steam.plugin/uninstall.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-dnf -y --setopt clean_requirements_on_remove=1 erase steam
-
-rpm --quiet --query ozon-repos-extra || rm -f /etc/yum.repos.d/fedora-steam.repo
+aptitude -y purge steam
