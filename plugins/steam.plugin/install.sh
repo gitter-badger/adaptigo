86512d1079cbff5583a2e96f9f07a771995c197b
Dick Thomas <xpd259@gmail.com>
2015-08-20 11:06:23 +0100

    first attempt at the steam plugin

--- plugins/steam.plugin/install.sh
+++ plugins/steam.plugin/install.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 
-dnf config-manager --add-repo=http://negativo17.org/repos/fedora-steam.repo
+FILE=/etc/apt/sources.list.backup
 
-dnf -y install steam
+if [ -f $FILE ];
+then
+   	echo "File $FILE exists"
+	aptitude update
+	aptitude install -y flashplugin-nonfree
+else
+	cp /etc/apt/sources.list /etc/apt/sources.list.backup
+	sed -i 's|debian/ jessie main|debian/ jessie main non-free contrib|g' /etc/apt/sources.list
 
-# Make "Big picture" mode work
-setsebool -P allow_execheap 1
+	sed -i 's|jessie/updates main|jessie/updates main non-free contrib|g' /etc/apt/sources.list
+
+	sed -i 's|debian/ jessie-updates main|debian/ jessie-updates main non-free contrib|g' /etc/apt/sources.list
+	aptitude update
+	aptitude install -y steam
+fi
+
+
+
+
+
