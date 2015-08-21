729f410efce4a85c40b643e76ae468aacef8da43
Dick Thomas <xpd259@gmail.com>
2015-08-21 16:40:15 +0100

    after experimenting found the numix stuff that works with debian there are some debs so need to fix that asap

--- plugins/numix.plugin/install.sh
+++ plugins/numix.plugin/install.sh
@@ -2,4 +2,4 @@
 echo "deb http://ppa.launchpad.net/numix/ppa/ubuntu vivid main" > /etc/apt/sources.list.d/numix.list
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0F164EEB
 aptitude update -y
-aptitude install -y  numix-icon-theme numix-gtk-theme numix-icon-theme-bevel 
+aptitude install -y  numix-icon-theme numix-icon-theme-circle numix-gtk-theme
--- plugins/numix.plugin/uninstall.sh
+++ plugins/numix.plugin/uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-aptitide -y purge numix-icon-theme numix-gtk-theme numix-icon-theme-bevel 
+aptitide -y purge numix-icon-theme numix-icon-theme-circle numix-gtk-theme
 rm /etc/apt/sources.list.d/numix.list
 aptitide update -y
