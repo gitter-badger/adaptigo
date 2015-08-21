cc12abd739b836ccb7927319261713406cf8385f
Dick Thomas <xpd259@gmail.com>
2015-08-21 16:29:18 +0100

    added keyserver

--- plugins/numix.plugin/install.sh
+++ plugins/numix.plugin/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "deb http://ppa.launchpad.net/numix/ppa/ubuntu vivid main" > /etc/apt/sources.list.d/numix.list
-gpg --export -a 0F164EEB | sudo apt-key add -
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0F164EEB
 aptitude install -y  numix-icon-theme numix-gtk-theme numix-folders  numix-icon-theme-bevel  numix-plank-theme  
