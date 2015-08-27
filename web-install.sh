7bdbd37cff493984f9135f58814679e02ecd7a71
Dick Thomas <xpd259@gmail.com>
2015-08-27 13:25:15 +0100

    more updates to webinstaller

--- web-install.sh
+++ web-install.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.#
+
+#    You should have received a copy of the GNU General Public License
+#   a long with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
 if [[ ! $(whoami) = "root" ]]; then
     echo "Please run the script as root."
     exit 1
 fi
-cd /opt/
+
+dir=`mktemp -d` && cd $dir
 url="https://github.com/xpd259/debbie/archive/v0.1-alpha.tar.gz"
 wgetboredom='--dns-timeout=30 --connect-timeout=30 --read-timeout=120 --tries=4'
 wgetprogress='--progress=bar:force --show-progress'
@@ -36,7 +53,7 @@ wget $wgetboredom $wgetprogress --no-check-certificate --content-disposition "$u
 [ "$wgetstat" != 0 ] && { print_wget_status $wgetstat; exit 1; }
 
 tar xf debbie-0.1-alpha.tar.gz
-
+cd debbie-0.1-alpha
 install -dm755 /usr/bin/
 install -dm755 /opt/debbie//
 
@@ -58,3 +75,5 @@ gtk-update-icon-cache -f -t /usr/share/icons/hicolor
 rm -Rfv /opt/debbie/plugins/
 install -dm755 /opt/debbie/plugins
 cp -pr plugins/* /opt/debbie/plugins/
+
+rm debbie-0.1-alpha.tar.gz
