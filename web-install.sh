b9bb69e2a662f571100d8a6f40fde380488778ff
Dick William Thomas <xpd259@gmail.com>
2015-08-29 18:46:44 +0100

    updated installer for v3

--- web-install.sh
+++ web-install.sh
@@ -22,7 +22,7 @@ if [[ ! $(whoami) = "root" ]]; then
 fi
 
 dir=`mktemp -d` && cd $dir
-url="https://github.com/xpd259/debbie/archive/v0.1-alpha.tar.gz"
+url="https://github.com/xpd259/debbie/archive/v0.3-alpha.tar.gz"
 wgetboredom='--dns-timeout=30 --connect-timeout=30 --read-timeout=120 --tries=4'
 wgetprogress='--progress=bar:force --show-progress'
 
@@ -50,9 +50,9 @@ options or .wgetrc or .netrc" ;;
 }
 
 wget $wgetboredom $wgetprogress --no-check-certificate --content-disposition "$url" 
-[ "$wgetstat" != 0 ] && { print_wget_status $wgetstat; exit 1; }
+[ "$wgetstat" != 0 ] && { print_wget_status $wgetstat; }
 
-tar xf debbie-0.1-alpha.tar.gz
-cd debbie-0.1-alpha
+tar xf debbie-0.3-alpha.tar.gz
+cd debbie-0.3-alpha
 ./debbie-installer
-rm debbie-0.1-alpha.tar.gz
+rm debbie-0.3-alpha.tar.gz
