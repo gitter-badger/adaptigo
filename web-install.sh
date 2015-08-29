d04b9d88f4e6160a6da60f12fe1ec183f9b16cfc
Dick William Thomas <xpd259@gmail.com>
2015-08-29 16:57:55 +0100

    updated webinstaller for v3 alpha

--- web-install.sh
+++ web-install.sh
@@ -22,7 +22,7 @@ if [[ ! $(whoami) = "root" ]]; then
 fi
 
 dir=`mktemp -d` && cd $dir
-url="https://github.com/xpd259/debbie/archive/v0.2-alpha.tar.gz"
+url="https://github.com/xpd259/debbie/archive/v0.3-alpha.tar.gz"
 wgetboredom='--dns-timeout=30 --connect-timeout=30 --read-timeout=120 --tries=4'
 wgetprogress='--progress=bar:force --show-progress'
 
@@ -53,7 +53,7 @@ wget $wgetboredom $wgetprogress --no-check-certificate --content-disposition "$u
 [ "$wgetstat" != 0 ] && { print_wget_status $wgetstat; }
 
 tar xf debbie-0.2-alpha.tar.gz
-cd debbie-0.2-alpha
+cd debbie-0.3-alpha
 ./debbie-installer
 cd ..
-rm debbie-0.2-alpha.tar.gz
+rm debbie-0.3-alpha.tar.gz  
