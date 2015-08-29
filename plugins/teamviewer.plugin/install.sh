520215e3bd5575fe13ecc51a19efadb18280b629
Dick William Thomas <xpd259@gmail.com>
2015-08-29 17:59:26 +0100

    it seems the teamviewer amd64 package is empty 0.o

--- plugins/teamviewer.plugin/install.sh
+++ plugins/teamviewer.plugin/install.sh
@@ -3,16 +3,11 @@
 aptitude install -y curl
 if [ `uname -m` = "x86_64" ]; then
  
-  # 64-bit stuff here
-wget http://download.teamviewer.com/download/teamviewer_amd64.deb
-dpkg -i teamviewer_amd64.deb
+dpkg --add-architecture
+# 32-bit stuff here
 
-
-else
-   # 32-bit stuff here
+fi
 wget http://download.teamviewer.com/download/teamviewer_i386.deb
 dpkg -i teamviewer_i386.deb
-fi
-
 apt-get --yes --fix-broken install
 
