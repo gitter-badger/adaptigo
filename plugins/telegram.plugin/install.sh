cc99bc3b9808fc7b97a5615c6705f5c67c6fef03
Dick Thomas <xpd259@gmail.com>
2015-08-29 10:56:02 +0100

    added desktop file for telegram

--- plugins/telegram.plugin/install.sh
+++ plugins/telegram.plugin/install.sh
@@ -1,7 +1,12 @@
 
 #!/bin/bash
 aptitude install -y curl
-if (uname -m  == 'x86_64'); then
+CACHEDIR="/var/cache/debi/Telegram";
+
+mkdir -p "$CACHEDIR"
+cd "$CACHEDIR"
+
+if [ `uname -m` = "x86_64" ]; then
  
   # 64-bit stuff here
   cd /opt
@@ -14,6 +19,20 @@ else
 
 
 fi
-  	tar xvf linux.tar.gz
-	rm linux.tar.gz -fv
-	/opt/Telegram/Telegram
+tar xvf linux.tar.gz
+
+cat <<EOF | tee /usr/share/applications/Telegram.desktop
+[Desktop Entry]
+Encoding=UTF-8
+Version=1.0
+Name=Telegram Desktop
+Comment=Official desktop version of Telegram messaging app
+Exec=/opt/Telegram/Telegram -- %u
+Icon=/home/dthomas/.TelegramDesktop/tdata/icon.png
+Terminal=false
+Type=Application
+Categories=Network;
+MimeType=application/x-xdg-protocol-tg;x-scheme-handler/tg;
+X-Desktop-File-Install-Version=0.22
+EOF
+
