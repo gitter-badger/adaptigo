fd44c739e937ead12d6e6889797ede51e4764376
Dick William Thomas <xpd259@gmail.com>
2015-08-29 16:55:29 +0100

    removed linux trck for now fixed stonking typo in codecs

--- plugins/codecs.plugin/install.sh
+++ plugins/codecs.plugin/install.sh
@@ -2,7 +2,9 @@
 aptitude install -y  software-properties-common
 cp /etc/apt/sources.list /etc/apt/sources.list.backup
 
-
+if ]; then
+	#statements
+fi
 grep  '^deb.*debian/ jessie.*main'  /etc/apt/sources.list | while read -r line ; do
     echo "Processing $line"
     apt-add-repository "$line  non-free contrib"
@@ -24,7 +26,7 @@ done
 
 mkdir -p /etc/debi/
 touch /etc/debi/sources.touched
-apt-add-repository 'apt-add-repositorydeb http://download.videolan.org/pub/debian/stable/ /'
+apt-add-repository 'deb http://download.videolan.org/pub/debian/stable/ /'
 wget -O - http://download.videolan.org/pub/debian/videolan-apt.asc | sudo apt-key add -
 aptitude update
 apt-get install -y libavcodec-extra libdvdcss2
